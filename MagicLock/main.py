8deb7079ab9a95237f3f01ccfb301fb96f7763f9
schultz77 <138313957+schultz77@users.noreply.github.com>
2023-11-01 07:16:46 +0100

    Add files via upload

---
+++ MagicLock/main.py
@@ -0,0 +1,131 @@
+from kivy.config import Config
+
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '900')
+
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+from kivymd.app import MDApp
+
+import screen_helper
+from kivy.lang import Builder
+
+import socket
+
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivy.uix.boxlayout import BoxLayout
+
+import ipaddress
+
+
+class Dialog(BoxLayout):
+    pass
+
+
+class LockApp(MDApp):
+    # layout_parent = ObjectProperty(None)
+    screen = None
+
+    def __init__(self, **kwargs):
+        super(LockApp, self).__init__(**kwargs)
+        self.serverAddress = ('xxx.xxx.xxx.xxx', 2222)
+        self.UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.UDPClient.setblocking(False)
+        self.bufferSize = 1024
+
+        self.theme_cls.primary_palette = "Teal"
+        self.theme_cls.primary_hue = '900'
+        self.theme_cls.material_style = "M2"
+        self.theme_cls.theme_style = "Dark"
+
+        self.dialog = None
+
+    def build(self):
+        self.screen = Builder.load_string(screen_helper.screen_helper)
+        return self.screen
+
+    def set_server(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Change Server Address:",
+                type="custom",
+                content_cls=Dialog(),
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=self.dialog_close
+                    ),
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=self.update_server
+                    ),
+                ],
+            )
+        self.dialog.content_cls.ids.sever_address.text = self.serverAddress[0]
+        self.dialog.content_cls.ids.sever_address.hint_text = "Server Address"
+        self.dialog.content_cls.ids.sever_address.text_color_normal = 'red'
+        self.dialog.open()
+
+    def dialog_close(self, *args):
+        self.dialog.dismiss(force=True)
+
+    def update_server(self, obj):
+
+        if self.is_ipv4(self.dialog.content_cls.ids.sever_address.text):
+            self.serverAddress = list(self.serverAddress)
+            self.serverAddress[0] = self.dialog.content_cls.ids.sever_address.text
+            self.serverAddress = tuple(self.serverAddress)
+            self.dialog_close()
+        else:
+            self.dialog.content_cls.ids.sever_address.hint_text = "wrong format!"
+            self.dialog.content_cls.ids.sever_address.text_color_normal = 'red'
+            self.dialog.content_cls.ids.sever_address.text = self.serverAddress[0]
+
+    @staticmethod
+    def is_ipv4(string):
+        try:
+            ipaddress.IPv4Network(string)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def exit_app():
+        LockApp().stop()
+
+    def lock_cmd(self):
+        cmd = 'unlock'
+        cmd_encoded = cmd.encode('utf-8')
+        self.UDPClient.sendto(cmd_encoded, self.serverAddress)
+
+    def temp_hum_cmd(self):
+        cmd = 'temperature'
+        cmd_encoded = cmd.encode('utf-8')
+        self.UDPClient.sendto(cmd_encoded, self.serverAddress)
+
+        cnt = 0
+        while cnt < 10:
+            try:
+                message, address = self.UDPClient.recvfrom(self.bufferSize)
+            except OSError:
+                pass
+            else:
+                message_decoded = message.decode('utf-8')
+                data = message_decoded.split(',')
+                temperature = data[0] + '°C'
+                humidity = data[1] + '%'
+
+                self.screen.ids.tf_temp.text = temperature
+                self.screen.ids.tf_hum.text = humidity
+            cnt += 1
+
+
+if __name__ == '__main__':
+    LockApp().run()
+
---
+++ MagicLock/screen_helper.py
@@ -0,0 +1,112 @@
+screen_helper = """
+#:import Window kivy.core.window.Window
+#:set color_shadow [0, 0, 0, .2980392156862745]
+
+<Dialog>
+    orientation: "vertical"
+    spacing: "12dp"
+    size_hint_y: None
+    height: "60dp"
+
+    MDTextField:
+        id: sever_address
+        hint_text: "Server Address"
+        icon_left: "server-network"
+        helper_text: "use ipv4 format (see current address)"
+        text_color_normal: 'red'
+
+
+
+MDBoxLayout:
+    id: layout_parent
+    layout_1: layout_child 
+    orientation: "vertical"
+
+    MDTopAppBar:
+        id: toolbar
+        # title: 'Magic Lock'
+        md_bg_color: app.theme_cls.primary_color
+        left_action_items: [["menu", lambda x: app.set_server()]]
+        right_action_items: [["exit-run", lambda x: app.exit_app()]]
+        elevation: 5
+
+    BoxLayout:
+        height: self.minimum_height
+        width: self.minimum_width
+        # spacing: dp(60)
+        size_hint_x: .8
+        pos_hint: {"center_x": .5, "center_y": .5}
+    
+        MDBoxLayout:
+            id: layout_child
+            tf_temp: tf_temp
+            tf_hum: tf_hum
+            orientation: "vertical"
+            spacing: "40dp"
+            adaptive_height: True
+            size_hint_x: .8
+            pos_hint: {"center_x": .5, "center_y": .5}
+            
+            Image:
+                source: 'images/image77.png'
+                pos_hint:{'center_x': 0.5, 'center_y': .9}
+                allow_stretch: True
+                keep_ratio: True
+                size_hint_y: None
+                size_hint_x: None
+                width: self.parent.width * 0.7
+                height: self.parent.width/self.image_ratio
+    
+            MDTextField:
+                id: tf_temp
+                mode: "round"
+                font_size: '30sp'
+                halign: 'right'
+                # hint_text: "Enter username"
+                helper_text: "temperature °C"
+                helper_text_mode: "persistent"
+                icon_left: "home-thermometer-outline"
+
+                icon_left_color: app.theme_cls.primary_color
+
+                # pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+                # size: Window.width - dp(20), "50dp"
+                # size_hint_x:1.5
+                # width:300
+                readonly: True
+    
+            MDTextField:
+                id: tf_hum
+                mode: "round"
+                font_size: '30sp'
+                halign: 'right'
+                # hint_text: "Enter username"
+                helper_text: "humidity %"
+                helper_text_mode: "persistent"
+                icon_left: "water-percent"
+                # icon: "eye-off"
+                icon_left_color: app.theme_cls.primary_color
+
+                readonly: True
+    
+    
+            MDIconButton:
+                id: update_button
+                icon: "cellphone-arrow-down"
+                pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+                icon_size: "48sp"
+                on_release:
+                    app.temp_hum_cmd()
+
+
+    MDBottomAppBar:
+        MDTopAppBar:
+            elevation: 1
+            # left_action_items: [["coffee", lambda x: app.navigation_draw()]]
+            mode: 'end'
+            type: 'bottom'
+            icon: "lock-open-variant"
+            on_action_button: app.lock_cmd()
+
+
+"""
